70a79e30dba9a93e0a91610cdb25e2eca88fc6a5
MrSkap <kozlovstepanvik@gmail.com>
2022-10-28 11:05:15 +0700

    mis in class name

--- ServiceEntities/ServiceStatus.cs
+++ ServiceEntities/ServiceStatus.cs
@@ -2,8 +2,14 @@
 
 namespace ServiseEntities;
 
-public class ServiseStatus
+public class ServiceStatus
 {
     public string Name { get; set; } = "name";
     public Health Health { get; set; } = Health.Degraded;
+
+    public ServiceStatus(string name, Health health)
+    {
+        Name = name;
+        Health = health;
+    }
 }
